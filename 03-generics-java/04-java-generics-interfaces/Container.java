849deac43f600610206acf4042a213c9f92dea64
Kody Samaroo <kody.samaroo@gmail.com>
2023-06-25 23:15:37 -0400

    java-generics-benefits

--- 03-generics-java/04-java-generics-interfaces/Container.java
+++ 03-generics-java/04-java-generics-interfaces/Container.java
@@ -1,20 +1,20 @@
 public class Container<T> implements Retriever<T> {
-    private T data;
-  
-    public Container(T data) {
-      this.data = data;
-    }
-  
-    public void setData(T data) {
-      this.data = data;
-    }
-  
-    public T getData() {
-      return this.data;
-    }
-  
-    @Override
-    public T retrieveData() {
-      return this.data;
-    }
+  private T data;
+
+  public Container(T data) {
+    this.data = data;
   }
+
+  public void setData(T data) {
+    this.data = data;
+  }
+
+  public T getData() {
+    return this.data;
+  }
+
+  @Override
+  public T retrieveData() {
+    return this.data;
+  }
+}
