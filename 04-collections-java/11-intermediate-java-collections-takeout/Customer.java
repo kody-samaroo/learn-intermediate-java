106d8e3047f4bc347e45c48df58c57f85355ae60
Kody Samaroo <kody.samaroo@gmail.com>
2023-06-29 18:58:15 -0400

    1/21 completed

---
+++ 04-collections-java/11-intermediate-java-collections-takeout/Customer.java
@@ -0,0 +1,22 @@
+// Create Customer class here
+public class Customer {
+    private String name;
+    private int money;
+  
+    public String getName(){
+      return this.name;
+    }
+  
+    public int getMoney(){
+      return this.money;
+    }
+  
+    public void setName(String name){
+      this.name = name;
+    }
+  
+    public void setMoney(int money){
+      this.money = money;
+    }
+  
+  }
